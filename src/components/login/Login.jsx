46d1bf4db08b26f14102aa4ed1c93d3dc2936030
TRANCHANT Marie <marie.tranchant@maif.fr>
2024-11-27 17:39:14 +0100

    navbar ok

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -4,14 +4,14 @@ const Login = () => {
     return (
       <div id="formContainer">
           {/*// TODO rajouter l'action action="/mapage"*/}
-          <form method="post" id="logInForm">
-              <div>
+          <form method="post" id="loginForm">
+              <div className="inputForm">
                   <label>Email</label>
-                  <input type="email" name="email"/>
+                  <input type="email" name="email" placeholder="Entrez votre adresse email"/>
               </div>
-              <div>
+              <div className="inputForm">
                   <label>Mot de passe</label>
-                  <input type="password"/>
+                  <input type="password" placeholder="Entrez votre mot de passe"/>
               </div>
               <button>Se connecter</button>
           </form>
--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,15 +1,18 @@
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faBars, faBell, faUser} from "@fortawesome/free-solid-svg-icons";
+import {faBars, faBell, faUser, faXmark} from "@fortawesome/free-solid-svg-icons";
 import "./navbar.scss"
 import {useState} from "react";
 
 const NavBar = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const hideMenu = () => setIsOpen(false);
+
     return (
         <div className="navBarContainer">
             <nav id="navBar" className={isOpen ? 'isOpen' : ''}>
-                <button id="burger" onClick={() => setIsOpen(!isOpen)}><FontAwesomeIcon icon={faBars}/></button>
+                <button id="burger" onClick={() => setIsOpen(!isOpen)} onBlur={hideMenu}><FontAwesomeIcon icon={faBars}/></button>
                 <div id="links">
+                    <button id="closeMenu"><FontAwesomeIcon icon={faXmark} /></button>
                     <a className="navBarLink">Découvrir les profils</a>
                     <a className="navBarLink">Messagerie</a>
                     <a className="navBarLink">Mes matchs</a>
