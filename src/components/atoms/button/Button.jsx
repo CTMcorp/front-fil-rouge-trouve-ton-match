79f264a4b896626f8e6a11e38f5ac56e376b613f
corantoine <coralie.antoine@maif.fr>
2025-02-05 15:12:30 +0100

    rollback

--- src/components/atoms/button/Button.jsx
+++ src/components/atoms/button/Button.jsx
@@ -1,12 +1,20 @@
-import("./button.scss");
+import {Link} from "react-router-dom";
+
+import ("./button.scss")
 
 // eslint-disable-next-line react/prop-types
-const Button = ({ text, type, onClick, className }) => {
-  return (
-    <button className={`buttons ${className}`} type={type} onClick={onClick}>
-      {text}
-    </button>
-  );
+const Button = ({text, type, onClick, className}) => {
+    return (
+        <div>
+            <button
+                className={`buttons ${className}`}
+                type={type}
+                onClick={onClick}
+            >
+                {text}
+            </button>
+        </div>
+    );
 };
 
 export default Button;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,50 +1,47 @@
-import "./home.scss";
+import "./home.scss"
 import Button from "../../components/atoms/button/Button.jsx";
-import { useNavigate } from "react-router-dom";
-import { useContext } from "react";
-import { AuthContext } from "../../config/AuthContext.jsx";
+import {Link} from "react-router-dom";
+import {useContext} from "react";
+import {AuthContext} from "../../config/AuthContext.jsx";
 
 const Home = () => {
-  const { isLogged } = useContext(AuthContext);
-  const navigate = useNavigate();
+    const {isLogged} = useContext(AuthContext);
 
-  const handleclick = () => {
-    navigate("/auth/login");
-  };
-
-  return (
-    <div id="container">
-      {!isLogged ? (
-        <Button onClick={handleclick} text="Se connecter"></Button>
-      ) : (
-        <div>
-          <h2>Bienvenue !</h2>
+    return (
+        <div id="container">
+            {!isLogged ? (
+                <Link to="/auth/login" style={{textDecoration: 'none'}}>
+                    <Button text="Se connecter"></Button>
+                </Link>
+            ) : <div>
+                <h2>Bienvenue !</h2>
+            </div>
+            }
+            <div id="content">
+                <div id="titleContent">
+                    <p id="title">Qu'est-ce que Trouve Ton Match ?</p>
+                </div>
+                <div id="description">
+                    <p>
+                        Trouve Ton Match est une application qui vous permettra
+                        de trouver LA personne pour vous accompagner et
+                        vous soutenir, une épaule sur laquelle vous reposer.
+                    </p>
+                    <p>
+                        Le but étant de créer un climat de confiance avec
+                        cette personne afin que vous puissiez vous tourner
+                        vers elle lorsque vous avez des soucis ou
+                        des besoins.
+                    </p>
+                    <p>
+                        Cette marraine / parrain peut être un chef d’entreprise
+                        ou un cadre dirigeant ayant de l’expérience et
+                        un réseau important pour faire appel à des partenaires experts dans vos besoins.
+                    </p>
+                </div>
+            </div>
         </div>
-      )}
-      <div id="content">
-        <div id="titleContent">
-          <p id="title">Qu'est-ce que Trouve Ton Match ?</p>
-        </div>
-        <div id="description">
-          <p>
-            Trouve Ton Match est une application qui vous permettra de trouver
-            LA personne pour vous accompagner et vous soutenir, une épaule sur
-            laquelle vous reposer.
-          </p>
-          <p>
-            Le but étant de créer un climat de confiance avec cette personne
-            afin que vous puissiez vous tourner vers elle lorsque vous avez des
-            soucis ou des besoins.
-          </p>
-          <p>
-            Cette marraine / parrain peut être un chef d’entreprise ou un cadre
-            dirigeant ayant de l’expérience et un réseau important pour faire
-            appel à des partenaires experts dans vos besoins.
-          </p>
-        </div>
-      </div>
-    </div>
-  );
-};
+    );
+}
 
 export default Home;
