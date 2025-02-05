2e666724f98a697a0011449a0e37c13a4c6380fb
corantoine <coralie.antoine@maif.fr>
2025-02-05 15:06:44 +0100

    fix: pointeur sur le bouton

--- src/components/atoms/button/Button.jsx
+++ src/components/atoms/button/Button.jsx
@@ -1,20 +1,12 @@
-import {Link} from "react-router-dom";
-
-import ("./button.scss")
+import("./button.scss");
 
 // eslint-disable-next-line react/prop-types
-const Button = ({text, type, onClick, className}) => {
-    return (
-        <div>
-            <button
-                className={`buttons ${className}`}
-                type={type}
-                onClick={onClick}
-            >
-                {text}
-            </button>
-        </div>
-    );
+const Button = ({ text, type, onClick, className }) => {
+  return (
+    <button className={`buttons ${className}`} type={type} onClick={onClick}>
+      {text}
+    </button>
+  );
 };
 
 export default Button;
