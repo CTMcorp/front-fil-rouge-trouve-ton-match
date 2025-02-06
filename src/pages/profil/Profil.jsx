a6d504ead5c736e11a6d4bfc09b025606422212c
corantoine <coralie.antoine@maif.fr>
2025-02-06 17:10:50 +0100

    fix: css responsive mobile

--- src/pages/profil/Profil.jsx
+++ src/pages/profil/Profil.jsx
@@ -12,7 +12,11 @@ const Profil = () => {
     <>
       <div id="content">
         <div id="profile-info">
-          <img alt="Photo de profil de l'utilisateur" src={profilPhoto} />
+          <img
+            className="profile-pic"
+            alt="Photo de profil de l'utilisateur"
+            src={profilPhoto}
+          />
           <div className="identity">
             <div className="user-info">
               <div className="name">
@@ -21,14 +25,17 @@ const Profil = () => {
               </div>
               <p>Métier</p>
             </div>
-            <div className="availabilities">Mes disponibilités</div>
+          </div>
+          <div className="availabilities">
+            <h3>Disponibilités :</h3>
+            <p>lundi, mardi, mercredi, jeudi</p>
           </div>
           <div className="needs">
             <p>Types de réseaux / besoins</p>
           </div>
           <div className="profile-btn">
             <FontAwesomeIcon icon={faTrashCan} onClick={handleClick} />
-            Supprimer mon profil
+            <p>Supprimer mon profil</p>
           </div>
         </div>
         <div className="profile-description">
@@ -40,7 +47,42 @@ const Profil = () => {
             aliquip ex ea commodo consequat. Duis aute irure dolor in
             reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
             pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+            culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in
+            voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+            officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit
+            amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+            nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+            esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+            cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum. Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+            esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+            cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum. Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+            esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+            cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum. Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+            esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+            cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum.
           </p>
         </div>
       </div>
