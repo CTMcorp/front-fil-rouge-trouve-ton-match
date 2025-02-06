b9907f7b59db513f1115617687deac6f43849dbd
Thibault Mesmin <thibault.mesmin@maif.fr>
2025-02-06 13:55:01 +0100

    feat: css files

--- src/pages/boiteOutil/BoiteOutil.jsx
+++ src/pages/boiteOutil/BoiteOutil.jsx
@@ -2,6 +2,20 @@ const BoiteOutil = () => {
     return (
         <div>
             <h1>Boite à outils</h1>
+            <div className="gestionDocument">
+                <button className="addDocument">
+                    <i className="fas fa-file-upload"></i>
+                    <span>Importer un document</span>
+                </button>
+                <button className="exportDocument">
+                    <i className="fas fa-file-download"></i>
+                    <span>Exporter un document</span>
+                </button>
+                <button className="deleteDocument">
+                    <i className="fas fa-trash-alt"></i>
+                    <span>Supprimer un document</span>
+                </button>
+            </div>
         </div>
     );
 }
