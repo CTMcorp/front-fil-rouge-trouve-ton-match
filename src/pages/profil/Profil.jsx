3ebc9ad1ac9dce58db1b67ae8cbe43889e5cdb9d
Thibault Mesmin <thibault.mesmin@maif.fr>
2025-04-04 16:10:18 +0200

    feat: design page profil

--- src/pages/profil/Profil.jsx
+++ src/pages/profil/Profil.jsx
@@ -1,68 +1,98 @@
 import "./profil.scss";
-import profilPhoto from "../../assets/profilPic.jpg";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
-import { Checkbox } from "../../components/atoms/checkbox/Checkbox";
- 
-import { useEmail, useFirstName, useLastName } from "../../services/infoUserDetails"
+import profilPhoto from "../../assets/user.png";
+
+import {
+    useEmail,
+    useFirstName,
+    useLastName,
+} from "../../services/infoUserDetails";
+import { useState } from "react";
 
 const Profil = () => {
-  return (
-    <div id="content">
-      <div id="profile-info">
-        <div className="profile-section profile-pic-section">
-          <img
-            className="profile-pic"
-            alt="Photo de profil de l'utilisateur"
-            src={profilPhoto}
-          />
-        </div>
-        <div className="profile-section user-info-section">
-          <div className="identity">
-            <div className="user-info">
-              <div className="name">
-                <p>Prénom: {useFirstName()}</p>
-                <p>Nom: {useLastName()}</p>
-                <p>Email: {useEmail()}</p>
-              </div>
-              <p className="job-title">Métier: </p>
+    const [edit, setEdit] = useState(false);
+
+    return (
+        <div id="content">
+            <div id="profile-info">
+                <div className="profile-section profile-pic-section">
+                    <img
+                        className="profile-pic"
+                        alt="Photo de profil de l'utilisateur"
+                        src={profilPhoto}
+                    />
+                </div>
+                <div className="profile-section user-info-section">
+                    <div className="identity">
+                        <div className="user-info">
+                            {edit ? (
+                                <>
+                                    <p>Prénom :</p>
+                                    <input
+                                        type="text"
+                                        value={useFirstName()}
+                                    ></input>
+                                    <p>Nom :</p>
+                                    <input
+                                        type="text"
+                                        value={useLastName()}
+                                    ></input>
+                                    <p>Email :</p>
+                                    <input
+                                        type="text"
+                                        value={useEmail()}
+                                    ></input>
+                                    <p>Métier :</p>
+                                    <input
+                                        type="text"
+                                        value={undefined}
+                                    ></input>
+                                    <p>Types de réseaux / besoins: </p>
+                                    <input
+                                        type="text"
+                                        value={undefined}
+                                    ></input>
+                                </>
+                            ) : (
+                                <>
+                                    <p>Prénom: {useFirstName()}</p>
+                                    <p>Nom: {useLastName()}</p>
+                                    <p>Email: {useEmail()}</p>
+                                    <p className="job-title">Métier: </p>
+                                    <p>Types de réseaux / besoins: </p>
+                                </>
+                            )}
+                        </div>
+                    </div>
+                </div>
+                <div className="profile-section profile-btn-section">
+                    <div className="profile-btn-delete">
+                        <button>Supprimer profil</button>
+                        <button onClick={() => setEdit(!edit)}>
+                            {edit ? "Enregistrer profil" : "Modifier profil"}
+                        </button>
+                    </div>
+                </div>
             </div>
-            <div className="needs">
-              <p>Types de réseaux / besoins</p>
+            <div className="profile-description">
+                <h3>Description du projet</h3>
+                {edit ? (
+                    <input type="text" value={undefined}></input>
+                ) : (
+                    <p>
+                        Le projet GreenTech Innovation vise à développer des
+                        solutions technologiques durables pour réduire
+                        l&apos;empreinte carbone des entreprises. En utilisant
+                        des énergies renouvelables et des technologies
+                        éco-responsables, le projet se concentre sur la création
+                        de systèmes énergétiques efficaces et la réduction des
+                        déchets industriels. L&apos;objectif principal est
+                        d&apos;aider les entreprises à adopter des pratiques
+                        plus vertes tout en restant compétitives sur le marché.
+                    </p>
+                )}
             </div>
-          </div>
         </div>
-        <div className="profile-section profile-btn-section">
-          <div className="profile-btn">
-            <FontAwesomeIcon icon={faTrashCan} />
-            <p>Supprimer profil</p>
-          </div>
-        </div>
-      </div>
-      <div className="availabilities">
-              <div>
-                <fieldset className="disponibilites">
-                  <legend>Disponibilités :</legend>
-                  <Checkbox name="scales" label="Lundi" />
-                  <Checkbox name="scales" label="Mardi" />
-                  <Checkbox name="scales" label="Mercredi" />
-                  <Checkbox name="scales" label="Jeudi" />
-                  <Checkbox name="scales" label="Vendredi" />
-                  <Checkbox name="scales" label="Samedi" />
-                </fieldset>
-              </div>
-            </div>
-      <div className="profile-description">
-        <h3>Description du projet</h3>
-        <p>
-        Le projet GreenTech Innovation vise à développer des solutions technologiques durables pour réduire l&apos;empreinte carbone 
-        des entreprises. En utilisant des énergies renouvelables et des technologies éco-responsables, 
-        le projet se concentre sur la création de systèmes énergétiques efficaces et la réduction des déchets industriels. 
-        L&apos;objectif principal est d&apos;aider les entreprises à adopter des pratiques plus vertes tout en restant compétitives sur le marché.
-        </p>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Profil;
