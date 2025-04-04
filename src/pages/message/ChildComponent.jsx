019e7fefb1b37ec9d6a8664c3f92876309b60184
corantoine <coralie.antoine@maif.fr>
2025-04-04 17:05:06 +0200

    WIP: messagerie

--- src/pages/message/ChildComponent.jsx
+++ src/pages/message/ChildComponent.jsx
@@ -1,16 +1,54 @@
-import React, { useState } from "react";
+import { Stomp } from "@stomp/stompjs";
+import React, { useEffect, useMemo, useState } from "react";
 import { useSubscription } from "react-stomp-hooks";
 
 const ChildComponent = () => {
-  const [message, setMessage] = useState("");
-  useSubscription("/topic/reply"),
-    (message) => {
-      setMessage(message.body);
-    };
+  //   const [message, setMessage] = useState("");
+  //   useSubscription("/topic/reply"),
+  //     (message) => {
+  //       setMessage(message.body);
+  //     };
+
+  const [loadMessage, setLoadMessages] = useState(false);
+  const [loader, setLoader] = useState(false);
+  const [messages, setMessages] = useState([]);
+  const client = useMemo(() => {
+    return Stomp.client("ws://localhost:8080/ws");
+  }, []);
+
+  useEffect(() => {
+    setLoader(true);
+    client.connect({}, () => {
+      client.subscribe("/getMessages", (e) => {
+        console.log("Receive Message", e.body);
+        setMessages(JSON.parse(e.body));
+      });
+      client.subscribe("/updateMessages", (e) => {
+        console.log("Update Message", e.body);
+      });
+      client.subscribe("/send", (e) => {
+        console.log("message sent", e.body);
+      });
+    });
+  }, [client, loadMessage, setMessages, setLoader]);
 
   return (
     <div>
-      <div>The broadcast message from websocket brocker is {message}</div>
+      <div>
+        <button
+          onClick={() => client.send("/requestMessages", {}, "{}")}
+          style={{ width: 10 }}
+        ></button>
+      </div>
+      <ul>
+        {messages.map((message) => (
+          <li key={message._id}>
+            <p>{message.sender}</p>
+            <p>{message.dest}</p>
+            <p>{message.content}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
